fd9a55bc9d1e6e782aa4cd91158c508266d02139
runner <runner@Mac-1702815715796.local>
2023-12-17 12:44:54 +0000

    Version bump: 2.2.0

--- Casks/espanso.rb
+++ Casks/espanso.rb
@@ -1,12 +1,12 @@
 cask "espanso" do
-  version "2.1.8"
+  version "2.2.0"
 
   if Hardware::CPU.intel?
     url "https://github.com/federico-terzi/espanso/releases/download/v#{version}/Espanso-Mac-Intel.zip"
-    sha256 "a17c03ff341beaf5a286aaab033dc443d703ed41f9dbfae653289dab00120bec"
+    sha256 "9a6f50e0b56b00e1d0d3f8db9967117b2e25a17ddb1d0945b8397339a2a1c08c"
   else
     url "https://github.com/federico-terzi/espanso/releases/download/v#{version}/Espanso-Mac-M1.zip"
-    sha256 "e84b5a96def1d793997fc8ee4e380f11b1d0515cf120402cd7aec6e07bafb74e"
+    sha256 "d7cc6392db6244d951d0543f8050656ff72d66e2c6d420e2b5e0aeddc484692b"
   end
 
   name "Espanso"
