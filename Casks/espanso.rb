ea418c1823f6dad4b4caa2a0a747944ea5e0a4b9
Auca Maillot <aucacoyan@gmail.com>
2025-09-27 17:57:41 -0300

    Deprecate the tap

--- Casks/espanso.rb
+++ Casks/espanso.rb
@@ -1,6 +1,9 @@
 cask "espanso" do
   version "2.2.1"
 
+  # Deprecate this custom tap's cask
+  deprecate! date: "2025-09-27", because: :moved_to_official_tap, replacement_formula: "espanso"
+
   if Hardware::CPU.intel?
     url "https://github.com/federico-terzi/espanso/releases/download/v#{version}/Espanso-Mac-Intel.zip"
     sha256 "369ad7eb9a30015a3836012970acd15b3b06c6f67349a89ced6bb3ae9c3f2d20"
@@ -13,18 +16,8 @@ cask "espanso" do
   desc "A Privacy-first, Cross-platform Text Expander"
   homepage "https://espanso.org/"
 
-  caveats do
-    <<~EOS
-      This tap is deprecated in favor of the official homebrew-cask tap.
-
-      You can safely untap this tap with the following command:
-      brew untap espanso/espanso
-
-      For more details visit: https://espanso.org/docs/install/mac/
-    EOS
-  end
-
   app "Espanso.app"
 
   zap trash: "~/Library/Caches/espanso"
 end
+
